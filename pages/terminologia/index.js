6a8a0c9b5fea87fe24b8298f090359eae71f7e62
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-08-26 01:18:31 -0300

    ajustes

--- pages/terminologia/index.js
+++ pages/terminologia/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import Terms from '../../components/Terms';
@@ -5,6 +6,15 @@ import Terms from '../../components/Terms';
 const Terminologia = () => {
   return (
     <>
+      <Head>
+        <title>Duna - Terminologia</title>
+        <meta
+          name="description"
+          content="Conheça alguns termos do universo de Duna"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#c54c07" />
+      </Head>
       <Navbar />
       <Terms />
       <Footer />
